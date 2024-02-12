6f59bbe44a242ab01a629b6691f1dd8df1bb619d
smartcode53 <smartcode53@gmail.com>
2024-02-12 09:30:15 +0400

    Added a string extension with a property that holds an empty string for convenience

---
+++ Sources/BreezeKit/StringExtension.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Taha Broachwala on 2/12/24.
+//
+
+import Foundation
+
+public extension String {
+    
+    /// An empty string
+    static var empty: String = ""
+}
+
+extension String: Error {
+    
+}
+
+

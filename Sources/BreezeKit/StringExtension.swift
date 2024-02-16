3b0a64c15f9aa3e046437232230a4230979c21fc
smartcode53 <smartcode53@gmail.com>
2024-02-16 19:59:09 +0400

    Added an extension to String to check whether an email is according to proper conventions and is valid.

--- Sources/BreezeKit/StringExtension.swift
+++ Sources/BreezeKit/StringExtension.swift
@@ -11,6 +11,22 @@ public extension String {
     
     /// An empty string
     static var empty: String = ""
+    
+    @available(iOS 16.0, *)
+    var isEmailValid: Bool {
+        let emailPattern = #"^([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$"#
+        
+        guard let emailRegex = try? Regex(emailPattern) else {
+            return false
+        }
+        
+        if self.firstMatch(of: emailRegex) == nil {
+            return false
+        }
+        
+        return true
+        
+    }
 }
 
 extension String: Error {
